84f93f713b63b94e209fa489ebc3495b7ca6f507
Aditya <leoadi246@gmail.com>
2019-09-24 23:05:40 +0530

    Initial commit

---
+++ add2numbers.java
@@ -0,0 +1,24 @@
+   /*     Experiment No. 1(b) 
+
+   Aim:-WAP to print addition of two numbers */
+
+
+
+import java.io.*;
+class add1
+{
+public static void main(String arg[])
+{
+int a=9,b=1;
+a+=b;
+System.out.println("sum="+a);
+}
+}
+
+
+/* C:\Users\Desktop\java>javac add1.java
+
+C:\Users\Desktop\java>java add1
+sum=10
+
+ */
